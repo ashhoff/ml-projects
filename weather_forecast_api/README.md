71d6139a7af4c7cba4cd8ca1bbe0d8e177eed3c2
ashhoff <ahoffric01@gmail.com>
2025-07-30 00:15:05 -0700

    Add real-time WeatherAPI integration with dotenv security and JSON parsing

---
+++ weather_forecast_api/README.md
@@ -0,0 +1,12 @@
+# Weather Forecast API
+Pulls real-time weather data from WeatherAPI and displays it in the terminal. Handles user input, external requests, and API key security using environment variables.
+
+## Highlights
+Live weather by city
+Clean terminal output
+API key management with .env
+
+## Stack
+Python
+WeatherAPI
+requests, python-dotenv
--- weather_forecast_api/weather_forcast.py
+++ weather_forecast_api/weather_forcast.py
@@ -1,5 +1,41 @@
 import requests
-api_key = "your_api_key_here"
-base_url = "http://api.weatherapi.com/v1/current.json"
-city = "Las Vegas"
-url = f"{base_url}?key={api_key}&q={city}"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("WEATHER_API_KEY")
+BASE_URL = "http://api.weatherapi.com/v1/current.json"
+
+def get_weather(city):
+    params = {
+        "key": API_KEY,
+        "q": city,
+        "aqi": "no"
+    }
+
+    try:
+        response = requests.get(BASE_URL, params=params)
+        response.raise_for_status()
+        data = response.json()
+        location = data["location"]["name"]
+        country = data["location"]["country"]
+        temp = data["current"]["temp_f"]
+        condition = data["current"]["condition"]["text"]
+
+        print(f"\n Location: {location}, {country}")
+        print(f" Temperature: {temp}°F")
+        print(f" Condition: {condition}")
+
+    except requests.exceptions.HTTPError as e:
+        print("HTTP error:", e)
+    except Exception as e:
+        print("Something went wrong:", e)
+
+
+mode = "dev"
+
+if mode == "prod":
+    get_weather("Bolivia")  # hardcoded test
+else:
+    city = input("Enter a city: ")
+    get_weather(city)
